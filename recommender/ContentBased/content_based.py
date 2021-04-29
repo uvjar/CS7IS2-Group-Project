aea6f90b5abc1128f7013d38f74c148e7abcde73
agjail <81770130+agjail@users.noreply.github.com>
2021-04-29 19:31:47 +0200

    Add files via upload

---
+++ recommender/ContentBased/content_based.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import os, sys
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+class ContentBased:
+    """Test calculates the similarity between movies"""
+    def __init__(self):
+        pass
+
+    def testPath(self):
+        print(os.getcwd())
+
+    def test(self, path):
+        movies = pd.read_csv(path+'/movies.csv', sep=',', encoding='latin-1')
+        genome_scores = pd.read_csv(path+'/genome-scores.csv', sep=',',
+                                    encoding='latin-1')
+
+        genome_scores_np = genome_scores.values
+        gen = [[0] * 1128] * int(len(genome_scores) / 1128)
+        gen_rel = [0] * 1128
+        movs = [0] * int(len(genome_scores) / 1128)
+        for m in range(0, int(len(genome_scores) / 1128)):
+            for tag in range(0, 1128):
+                gen_rel[tag] = genome_scores_np[1128 * m + tag][2]
+            movs[m] = genome_scores_np[m * 1128][0]
+            gen[m] = gen_rel[:]
+
+        # compute the cosine similarity for all movies against all movies
+        cos_sim = cosine_similarity(gen)
+
+        id_movie_to_match = 0
+
+        idx_matched_top5 = np.argsort(cos_sim[id_movie_to_match])[-6:-1]
+
+        j = 0
+        movie_recommended = [id_movie_to_match] * 5
+        for i in idx_matched_top5:
+            idx = movies['title'].get(movies['movieId'] == movs[i])
+            movie_recommended[j] = idx.values[0]
+            #print(i, cos_sim[i][0])
+            j += 1
+        print(movie_recommended)
+
+        # user profiles
+        #users, rated = np.unique(ratings['userId'].values, return_counts=True)  # only ok movies are selected
+        # movies_to_gen_pred = 50
+        users_rated = [[0] * 1128] * len(users)
+        #counter = 0  # incremented by +rated by user i
+        #for i in range(0, len(users)):  # for each user
+        #    user_id = users[i]
+        #    rated_movies = rated[i]
+        #    user_tags = [[0] * 1128] * rated_movies
+        #    for j in range(0, rated_movies):  # get averaged genome tags relevance
+        #        movie_id = ratings['movieId'][counter]
+        #        counter += 1
+        #        # get the row of that movie id, if the movie has available genome tags
+        #        if movie_id in movs:
+        #            m = movs.index(movie_id)
+        #            user_tags[j] = gen[m]
+        #    # for each user create averaged genome tag relevance genome_tag
+        #    user_tags = np.array(user_tags)
+
+
+
+
+
+
+
